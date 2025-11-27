718f7380d8c8defd8f7173ac03d30f83dbacd822
Shakthi Senavirathna <shakthizen101@gmail.com>
2025-11-27 15:48:46 +0530

    Implement zot screen guard

--- lib/zot_screen_guard.dart
+++ lib/zot_screen_guard.dart
@@ -1,5 +1,36 @@
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'dart:developer';
+import 'dart:io';
+
+import 'package:flutter/foundation.dart';
+import 'package:no_screenshot/no_screenshot.dart';
+import 'package:screenshot_guard/screenshot_guard.dart';
+
+class ZOTScreenGuard {
+  static Future<void> disableScreenCapture() async {
+    if (kIsWeb) return;
+    try {
+      if ((Platform.isAndroid || Platform.isIOS || Platform.isMacOS)) {
+        await NoScreenshot.instance.screenshotOff();
+      } else if (Platform.isWindows) {
+        await ScreenshotGuard().enableSecureFlag(enable: true);
+      }
+    } catch (e) {
+      log("Screenshot prevention not supported");
+      log(e.toString());
+    }
+  }
+
+  static Future<void> enableScreenCapture() async {
+    if (kIsWeb) return;
+    try {
+      if ((Platform.isAndroid || Platform.isIOS || Platform.isMacOS)) {
+        await NoScreenshot.instance.screenshotOn();
+      } else if (Platform.isWindows) {
+        await ScreenshotGuard().enableSecureFlag(enable: false);
+      }
+    } catch (e) {
+      log("Screenshot prevention not supported");
+      log(e.toString());
+    }
+  }
 }
